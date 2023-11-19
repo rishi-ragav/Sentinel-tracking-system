e878d56f10a31aeefd9439f2265700cf24ae1711
Rishi Ragav <rishiragav1@gmail.com>
2023-11-19 23:04:03 +0530

    updated files

--- testing/Test 2/test2.py
+++ testing/Test 2/test2.py
@@ -1,7 +1,7 @@
 import cv2
 from ultralytics import YOLO
 #from ultralytics.yolo.v8.detect.predict import DetectionPredictor
-from ultralytics import YOLO
+
 import torch
 import cv2 
 import numpy as np
@@ -10,5 +10,5 @@ import matplotlib.pyplot as plt
 
 
 
-model = YOLO("D:/Downloads/best.pt")
-model.predict(source="0", show=True, conf=0.5)
+model = YOLO("D:/OneDrive/OneDrive - Student Ambassadors/College notes/Sem 7/github/Sentinel-tracking-system/testing/Test 2/best.pt")
+model.predict(source="0", show=True, conf=0.75)
